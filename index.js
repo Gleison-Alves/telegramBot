a63cd61249ba9cf96fe9566330e969945b94e91c
Gleison Alves <hukinalves9@gmail.com.br>
2021-06-19 22:29:30 -0300

    tentando colocar a funcionalidade de mostrar quando dias faltam para o natal

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '1856550109:AAEHMOGptiW1WeYAyZA4gTsTcq3qCnutiwU';
 
+// const stream = bot.sendAudio(chatId, '/Music/musica/Circle.mp3');
+
+// const bot = new TelegramBot(token, {
+//     filepath: false,
+//   });
+
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message',(msg) =>{
     const text = msg.text;
 
     var hi = "oi";
-    if(text.toString().toLowerCase().indexOf(hi) === 0){
+    if(text.toString().toLowerCase().includes(hi)){
         bot.sendMessage(msg.chat.id,"Ola "+ msg.from.first_name);
     }
     var tchau = "tchau";
@@ -27,6 +33,26 @@ bot.on('message',(msg) =>{
         bot.sendMessage(msg.chat.id, "Aqui está á localidade de São Paulo");
 
     }
+
+    var natal = "natal";
+    if(msg.text.toString().toLowerCase().includes(natal)){
+        dia = new Date();
+        var cmas = new Date(dia.getFullYear(), 11, 25);
+
+        if(dia.getMonth() == 11 && dia.getDate() > 25){
+            cmas.setFullYear(cmas.getFullYear()+1);
+        }
+
+        var one_day = 1000*60*60*24;
+        var h = Math.ceil((cmas.getTime()-dia.getTime())/(one_day));
+        bot.sendMessage(msg.chat.id,h +" dias restantes até o natal")
+    }
+
+//    var alerta = "alerte";
+//    if(text.toLowerCase().includes(alerta)){
+//         bot.sendAudio(chatId, stream);
+//    }
+   
     //para grupo se alguem fala idiota o bot remove este
     // var what = "idiota";
     // if (msg.text.includes(what)) {
@@ -39,7 +65,6 @@ bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(msg.chat.id, "seja bem-vindo(a)");
  });
-
 
 bot.onText(/\/sobre/, (msg) => {
 
